a351631168040623fa5b8bfbe069204a00d0e3aa
Manish Kumar <manish.login01@gmail.com>
2026-02-28 23:49:57 +0530

    re-initial commit

---
+++ frontend/app/layout.js
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "TasteTrove - AI Recipes Platform",
+  description: "TasteTrove is an AI-powered recipe platform that helps you discover new recipes and create your own",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        {/* Footer */}
+        <footer className="py-8 px-4 border-t">
+          <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
+            <div className="flex items-center gap-3">
+              <Image src="/logo.png" alt="TasteTrove Logo" width={48} height={48} className="w-14" />
+            </div>
+            <p className="text-stone-500 text-sm">Made with 💗 by Manish Kumar</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
