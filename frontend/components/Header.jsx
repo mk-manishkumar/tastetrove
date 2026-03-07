4ff2458b02e940bc66ad978da4e98ec9b9f35f7f
Manish Kumar <manish.login01@gmail.com>
2026-03-08 00:28:03 +0530

    feat : Implement application header with navigation & Clerk authentication UI

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Button } from './ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Cookie, Refrigerator } from 'lucide-react'
+import UserDropdown from './UserDropdown'
 
-const Header = () => {
+const Header = async() => {
+  const user = null;
   return (
     <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-        Logo
+        {/* Logo */}
+        <Link href={user ? "/dashboard" : "/"}>
+          <Image src="/orange-logo.png" alt="TasteTrove Logo" width={60} height={60} className="w-16" />
+        </Link>
 
-        <div> Nav Links </div>
-        
+        {/* Navigation Links */}
+        <div className="hidden md:flex items-center space-x-8 text-sm font-medium text-stone-600">
+          <Link href="/recipes" className="hover:text-orange-600 transition-colors flex gap-1.5 items-center">
+            <Cookie className="w-4 h-4" />
+            My Recipes
+          </Link>
+          <Link href="/pantry" className="hover:text-orange-600 transition-colors flex gap-1.5 items-center">
+            <Refrigerator className="w-4 h-4" />
+            My Pantry
+          </Link>
+        </div>
+
+        {/* Auth buttons */}
         <div className='flex items-center space-x-4'>
           {/* Show the user button when the user is signed in */}
         <SignedIn>
-          <UserButton />  
+          <UserDropdown />  
         </SignedIn>
         {/* Show the sign in and sign up buttons when the user is signed out */}
             <SignedOut>
