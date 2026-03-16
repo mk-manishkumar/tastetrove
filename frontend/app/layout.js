e6644971a10e05c78827e03d68526c64dc05aa17
Manish Kumar <manish.login01@gmail.com>
2026-03-16 22:38:33 +0530

    feat : Add global application layout with authentication and a versatile RecipeCard component supporting multiple display variants.

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Image from "next/image";
 import { ClerkProvider } from "@clerk/nextjs";
 import { neobrutalism } from "@clerk/themes";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,22 +15,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{baseTheme: neobrutalism}}>
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
-        <Header />
-        <main className="min-h-screen">{children}</main>
-        {/* Footer */}
-        <footer className="py-8 px-4 border-t">
-          <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
-            <div className="flex items-center gap-3">
-              <Image src="/orange-logo.png" alt="TasteTrove Logo" width={48} height={48} className="w-14" />
+    <ClerkProvider appearance={{ baseTheme: neobrutalism }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} antialiased`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+          {/* Footer */}
+          <footer className="py-8 px-4 border-t">
+            <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
+              <div className="flex items-center gap-3">
+                <Image src="/orange-logo.png" alt="TasteTrove Logo" width={48} height={48} className="w-14" />
+              </div>
+              <p className="text-stone-500 text-sm">Made with 💗 by Manish Kumar</p>
             </div>
-            <p className="text-stone-500 text-sm">Made with 💗 by Manish Kumar</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
